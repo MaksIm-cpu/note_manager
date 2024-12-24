ae14c3ef652776687cb1703d591dd090213f8a13
MaksIm-cpu <maksimkozhin0209@gmail.com>
2024-12-24 15:34:45 +0300

    Add files via upload

---
+++ Grade1 1.1.py
@@ -0,0 +1,14 @@
+username = 'Maksim'
+title = 'New'
+content = 'project start'
+status = 'active'
+created_date = "23-12-2024"
+issue_date = "10-01-2025"
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
+
